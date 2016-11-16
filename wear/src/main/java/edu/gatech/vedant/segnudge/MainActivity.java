b554fe5682d393a0328ef32bd34c695d0a877ee7
vedantswain <vedant12118@iiitd.ac.in>
2016-11-15 23:51:28 -0500

    disabled exit + long press exit + swipe detection

--- wear/src/main/java/edu/gatech/vedant/segnudge/MainActivity.java
+++ wear/src/main/java/edu/gatech/vedant/segnudge/MainActivity.java
@@ -3,13 +3,23 @@ package edu.gatech.vedant.segnudge;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.wearable.view.DismissOverlayView;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
     private static final String TAG = "MainActivity";
+
+    private static final int SWIPE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 50;
+
+    private DismissOverlayView mDismissOverlay;
+    private GestureDetector mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +34,69 @@ public class MainActivity extends Activity {
         });
 
         startService(new Intent(this, DataService.class));
+
+        // Configure a gesture detector
+        mDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public void onLongPress(MotionEvent event) {
+                Log.d(TAG, " onLongPress: " + event.toString());
+                finish();
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                onSwipeRight();
+                            } else {
+                                onSwipeLeft();
+                            }
+                        }
+                        result = true;
+                    }
+                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeBottom();
+                        } else {
+                            onSwipeTop();
+                        }
+                    }
+                    result = true;
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        });
+
+    }
+
+    // Capture long presses
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+//        Log.d(TAG,"Touch event");
+        return mDetector.onTouchEvent(ev) || super.onTouchEvent(ev);
+    }
+
+    public void onSwipeRight() {
+        Log.d(TAG, "Swipe Right");
+    }
+
+    public void onSwipeLeft() {
+        Log.d(TAG, "Swipe Left");
+    }
+
+    public void onSwipeTop() {
+        Log.d(TAG, "Swipe Top");
+    }
+
+    public void onSwipeBottom() {
+        Log.d(TAG, "Swipe Bottom");
     }
 }
