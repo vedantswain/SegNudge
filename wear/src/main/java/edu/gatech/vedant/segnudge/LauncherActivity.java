4261e12df937f573635579792b90bbe4aca43e70
vedantswain <vedant12118@iiitd.ac.in>
2016-11-17 01:10:32 -0500

    added emoji and hints

--- wear/src/main/java/edu/gatech/vedant/segnudge/LauncherActivity.java
+++ wear/src/main/java/edu/gatech/vedant/segnudge/LauncherActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
@@ -31,25 +32,32 @@ public class LauncherActivity extends Activity {
     private final String YES="y";
     private final String NO="n";
 
-    private String initProbe="pb";
+    private String initProbe;
     private String currProbe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
-//        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-//        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-//            @Override
-//            public void onLayoutInflated(WatchViewStub stub) {
-//                mTextView = (TextView) stub.findViewById(R.id.text);
-//            }
-//        });
 
         startService(new Intent(this, DataService.class));
 
-        //Plastic tree
-        mMap =Common.getPbMap(this);
+//        //Plastic tree
+//        mMap =Common.getPbMap(this);
+//        initProbe="pb";
+//
+//        //Aluminum tree
+//        mMap =Common.getAcMap(this);
+//        initProbe="ac";
+
+        //Paper tree
+        mMap =Common.getMpMap(this);
+        initProbe="mp";
+
+//        //Landfill tree
+//        mMap =Common.getLfMap(this);
+//        initProbe="lf";
+
         currProbe=initProbe;
 
         // Configure a gesture detector
@@ -67,30 +75,10 @@ public class LauncherActivity extends Activity {
     private void configFlipper() {
         mViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
 
-        mViewFlipper.addView(getNewView(mMap.get("pb")));
+        mViewFlipper.addView(getRootView(mMap.get(currProbe)));
 //        mViewFlipper.addView(getNewView("End World"));
     }
 
-    public View getNewView(String question){
-        RelativeLayout rl = new RelativeLayout(this);
-        RelativeLayout.LayoutParams rlParams =
-                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-        rl.setLayoutParams(rlParams);
-
-        TextView textView = new TextView(this);
-        textView.setText(question);
-        RelativeLayout.LayoutParams layoutParams =
-                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
-        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
-        textView.setLayoutParams(layoutParams);
-
-        rl.addView(textView);
-
-        return rl;
-    }
 
     public void configDetector(){
         mDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
@@ -185,6 +173,105 @@ public class LauncherActivity extends Activity {
        return Arrays.asList(Common.leafProbes).contains(probe);
     }
 
+    public View getNewView(String question){
+        RelativeLayout rl = new RelativeLayout(this);
+        RelativeLayout.LayoutParams rlParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        rl.setLayoutParams(rlParams);
+
+        TextView textView = new TextView(this);
+        textView.setText(question);
+        RelativeLayout.LayoutParams layoutParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        textView.setLayoutParams(layoutParams);
+
+        textView.setTextAppearance(this,android.R.style.TextAppearance_DeviceDefault_Medium);
+
+        rl.addView(textView);
+
+        return rl;
+    }
+
+    public View getRootView(String question){
+        RelativeLayout rl = new RelativeLayout(this);
+        RelativeLayout.LayoutParams rlParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        rl.setLayoutParams(rlParams);
+
+        TextView textView = new TextView(this);
+        textView.setText(question);
+        RelativeLayout.LayoutParams layoutParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        textView.setLayoutParams(layoutParams);
+
+        textView.setTextAppearance(this,android.R.style.TextAppearance_DeviceDefault_Medium);
+
+        rl.addView(textView);
+
+        TextView noView = new TextView(this);
+        noView.setText("No");
+        RelativeLayout.LayoutParams noParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        noParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        noParams.addRule(RelativeLayout.ALIGN_PARENT_START);
+        noParams.setMargins(10,0,0,0);
+        noView.setLayoutParams(noParams);
+
+        int noId = View.generateViewId();
+        noView.setId(noId);
+
+        rl.addView(noView);
+
+        TextView yesView = new TextView(this);
+        yesView.setText("Yes");
+        RelativeLayout.LayoutParams yesParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        yesParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        yesParams.addRule(RelativeLayout.ALIGN_PARENT_END);
+        yesParams.setMargins(0,0,10,0);
+        yesView.setLayoutParams(yesParams);
+
+        int yesId = View.generateViewId();
+        yesView.setId(yesId);
+
+        rl.addView(yesView);
+
+        ImageView swipeRight = new ImageView(this);
+        swipeRight.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_swipe));
+        RelativeLayout.LayoutParams rightParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        rightParams.addRule(RelativeLayout.END_OF,noId);
+        rightParams.addRule(RelativeLayout.ALIGN_TOP,noId);
+        swipeRight.setLayoutParams(rightParams);
+
+        rl.addView(swipeRight);
+
+        ImageView swipeLeft = new ImageView(this);
+        swipeLeft.setImageDrawable(getResources().getDrawable(R.drawable.ic_left_swipe));
+        RelativeLayout.LayoutParams leftParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        leftParams.addRule(RelativeLayout.START_OF,yesId);
+        leftParams.addRule(RelativeLayout.ALIGN_TOP,yesId);
+        swipeLeft.setLayoutParams(leftParams);
+
+        rl.addView(swipeLeft);
+
+        return rl;
+    }
+
+
     public View finalLeaf(String probe){
         BoxInsetLayout bil = (BoxInsetLayout) findViewById(R.id.watch_view_stub);
         bil.setBackgroundColor(getResources().getColor(R.color.colorSuccess));
@@ -210,10 +297,43 @@ public class LauncherActivity extends Activity {
         layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
         textView.setLayoutParams(layoutParams);
 
+        textView.setTextAppearance(this,android.R.style.TextAppearance_DeviceDefault_Medium);
         textView.setTextColor(getResources().getColor(R.color.colorTextSuccess));
 
         rl.addView(textView);
 
+        TextView emojiView = new TextView(this);
+        emojiView.setText(getEmoji(probe));
+        RelativeLayout.LayoutParams emojiParams =
+                new RelativeLayout.LayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        emojiParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        emojiParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        emojiView.setLayoutParams(emojiParams);
+
+        emojiView.setTextAppearance(this,android.R.style.TextAppearance_DeviceDefault_Large);
+
+        rl.addView(emojiView);
+
         return rl;
+    }
+
+    public String getEmoji(String probe){
+        int emojiInt;
+        String probeText=mMap.get(probe);
+        if(probeText.contains("another"))
+            emojiInt=R.integer.another;
+        else if(probeText.contains("Landfill"))
+            emojiInt=R.integer.landfill;
+        else if (probe.contains("pb"))
+            emojiInt=R.integer.plastic;
+        else if (probe.contains("ac"))
+            emojiInt=R.integer.aluminum;
+        else if (probe.contains("mp"))
+            emojiInt=R.integer.paper;
+        else
+            emojiInt=R.integer.landfill;
+
+        return new String(Character.toChars(getResources().getInteger(emojiInt)));
     }
 }
