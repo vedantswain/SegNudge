45dc404f3d39cd0dea0a19ff9fba50450fbf06d9
jesadjus <jesadjust@gmail.com>
2016-11-29 16:46:30 -0500

    combined script

--- GUIfornudge/main.py
+++ GUIfornudge/main.py
@@ -5,9 +5,12 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
-from kivy.properties import StringProperty, NumericProperty, ObjectProperty, ListProperty
+# from kivy.properties import StringProperty, NumericProperty, ObjectProperty, ListProperty
 kivy.require('1.9.0')
 import time
+import sys
+import json
+import requests
 
 timenow = time.strftime("%H:%M:%S")
 
@@ -63,15 +66,45 @@ class GUI(GridLayout):
     def fire(self, obj):
         print(timenow +","+"Aluminum")
         f.write(timenow +","+'Aluminum\n')
+        self.push("aluminum")
     def fire2(self, obj):
         print(timenow +","+"fire Plastic")
         f.write(timenow +","+'Plastic\n')
+        self.push("plastic")
     def fire3(self, obj):
         print(timenow +","+"fire Paper")
         f.write(timenow +","+'Paper\n')
+        self.push("paper")
     def fire4(self, obj):
         print(timenow +","+"fire Landfill")
         f.write(timenow +","+'Landfill\n')
+        self.push("landfill")
+
+    def push(self, bintype):
+        url="https://fcm.googleapis.com/fcm/send"
+        server_key="AIzaSyB3QhKVX5vEVa4VM7Wb0WvNuIH469HNZJA"
+        alpha=10
+        headers = {'Content-type': 'application/json', 'Authorization': 'key='+server_key }
+        topic = "/topics/research-intervention"
+        reg_ids = ["d65g_WO0z18:APA91bHxuB07aSpJyhOoMyhm087ZRLlWsl6Gym5DCA1OBvWNAVLWCXuGKRa1vBr4OlhrmrAJPgZ8n5Roi1kEHCcsiBJ1pFxk6TQjVKhE_oEijqlGXBNlLnkcvCC08zdvKvP1BmjQkMV8"]
+
+        # bin_type=sys.argv[1]
+
+        data = {
+            "bin_type" : bintype,
+        }
+
+        body={
+            "data": data,
+            "registration_ids": reg_ids,
+            # "to": "/topics/"+sys.argv[1]
+        }
+
+        r = requests.post(url, data=json.dumps(body), headers=headers)
+
+        print(r.status_code, r.reason)
+        print(r.text + '...')
+        # print (body)
 
 
 class Main(App):
